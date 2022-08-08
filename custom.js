1e0eafb1482ae96579c401b65266b898bb5461ef
Humayun <hkobir009@gmail.com>
2022-08-08 18:18:45 +0600

    change html content , change html attr , js data show on desplay

---
+++ custom.js
@@ -0,0 +1,57 @@
+  
+  //  (part-3)  javascript will show Output 
+
+// function myFunction(){
+//    document.getElementById('test').innerHTML = ' I am Not kabir';
+   
+// }
+// function myFunction(){
+//     document.write(11);
+// }
+
+// function myFunction(){
+//     console.log('i am  kabir from script');
+// }
+
+// function myFunction(){
+//     // window.alert(11);
+//     // or
+//     alert(11);
+// }
+  
+
+
+
+
+  
+//  (part-2)  html can change html attribute  
+
+// function myFunction(){
+//    document.getElementById('image').src = 'pic_bulbon.gif';
+   
+// }
+// function myFunction1(){
+//    document.getElementById('image').src = 'pic_bulboff1.gif';
+// }
+// function myFunction2(){
+//    document.getElementById('paragraph').style.color = 'red';
+// }
+// function myFunction3(){
+//    document.getElementById('paragraph').style.fontSize = '20px';
+// }
+// //  set attribute multiple style
+// function myFunction4(){
+//    let obj = document.getElementById('paragraph');
+//    obj.setAttribute("style", "font-size: 20px; color: red; background-color: lightgray;");
+// }
+
+  
+  
+  
+  
+  
+  // (Part-1)  javascript can change html content 
+
+    // function myFunction(){
+    //     document.getElementById('name').innerHTML = 'hay , I am humayun Kabir';
+    // }
